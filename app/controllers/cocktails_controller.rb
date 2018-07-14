8e01ceb7bbeb20f3ef1a5fbadd2efcd3118d3736
SEZEN ULUHANSIN <sezenuluhansin@hotmail.com>
2018-07-14 19:45:52 +0300

    rails new

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,32 @@
+class CocktailsController < ApplicationController
+
+  def index
+   @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
+  end
+
+  def new
+    @cocktail = Cocktail.new(params[:cocktails_id])
+  end
+
+  def create
+
+   @cocktail = Cocktail.new(cocktail_params)
+   if @cocktail.save
+
+    redirect_to cocktail_path(@cocktail)
+   else
+    render 'new'
+   end
+  end
+   private
+
+   def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,20 @@
+class ReviewsController < ApplicationController
+  def create
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @reivew = Review.new (review_params)
+    @review.cocktail = @cocktail
+    if @review.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      @dose = Dose.new
+      render "cocktails/show"
+    end
+  end
+
+
+   private
+
+   def review_params
+    params.require(:review).permit(:rating,:contect)
+  end
+end
